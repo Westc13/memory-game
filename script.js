2d2bb7e75ea30b1819716b77fc8a630153e47b57
ChristineWest <christinewwwest@gmail.com>
2022-10-20 16:31:21 -0400

    use setAttribute to add src img to cardId clicked

--- script.js
+++ script.js
@@ -75,4 +75,5 @@ function flipCard() {
   cardsChosen.push(cardArray[cardId].name);
   console.log("clicked", cardId);
   console.log(cardsChosen);
+  this.setAttribute("src", cardArray[cardId].img);
 }
